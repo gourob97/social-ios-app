aaee1bb50e7e7192fad39c75db971c9cdda9b7a1
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2025-10-28 01:49:31 +0600

    Initial Commit

---
+++ Social/SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialApp.swift
+//  Social
+//
+//  Created by Gourob Mazumder on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
